f0c28f639e1e1f402f57a691c4e353f8080145d2
brigss <brigitta.rucz@yahoo.com>
2023-04-26 18:20:18 +0300

    Add DetailsView with styling and column calculus

--- src/router/pages/components/details/DetailsView.tsx
+++ src/router/pages/components/details/DetailsView.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
-import Modal from "router/utils/Modal";
+import { useEffect, useRef, useState } from "react";
+import ComplexModal from "router/utils/ComplexModal";
+import GenericModal from "router/utils/GenericModal";
 import { List, Page } from "services/database/models";
+import FooterView from "../footer/FooterView";
 import AddView from "../form/AddView";
 import EditView from "../form/EditView";
+import RemoveView from "../form/RemoveView";
 import ListView from "./components/ListView";
-import PageView from "./components/PageView";
+import SubPageView from "./components/SubPageView";
 
 interface Props {
   resListArray: List[];
@@ -14,6 +17,19 @@ interface Props {
 const DetailsView = ({ resListArray, resPage }: Props) => {
   const [isEditViewVisible, setIsEditViewVisible] = useState(false);
   const [isAddViewVisible, setIsAddViewVisible] = useState(false);
+  const [isRemoveViewVisible, setIsRemoveViewVisible] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const refContainer = useRef(null);
+
+  const refFirstColumn = useRef(null);
+  const refSecondColumn = useRef(null);
+  const refThirdColumn = useRef(null);
+
+  // TODO: add throttling
+  // TODO: add skeleton loading
+  window.addEventListener("resize", () => {
+    setWindowWidth(window.innerWidth);
+  });
 
   const add = () => {
     setIsAddViewVisible(true);
@@ -23,11 +39,97 @@ const DetailsView = ({ resListArray, resPage }: Props) => {
     setIsEditViewVisible(true);
   };
 
+  const remove = () => {
+    setIsRemoveViewVisible(true);
+  };
+
+  useEffect(() => {
+    if (windowWidth < 1250) {
+      return;
+    }
+
+    const template = refContainer.current;
+
+    if (!template) {
+      return;
+    }
+
+    const articles = (template as HTMLDivElement).querySelectorAll(
+      ".articles_template-item"
+    );
+
+    const firstColumn = refFirstColumn.current;
+    const secondColumn = refSecondColumn.current;
+    const thirdColumn = refThirdColumn.current;
+
+    if (!firstColumn || !secondColumn || !thirdColumn) {
+      return;
+    }
+
+    for (let n = 0; n < articles.length; n++) {
+      let smallestColumnHeight = -1;
+      let biggestColumnHeight = -1;
+
+      const heightsArray = [
+        (firstColumn as HTMLDivElement).getBoundingClientRect().height,
+        (secondColumn as HTMLDivElement).getBoundingClientRect().height,
+        (thirdColumn as HTMLDivElement).getBoundingClientRect().height,
+      ];
+
+      heightsArray.forEach((element) => {
+        if (biggestColumnHeight === -1) {
+          biggestColumnHeight = element;
+        } else if (biggestColumnHeight < element) {
+          biggestColumnHeight = element;
+        }
+      });
+
+      const biggestColumnHeightIndex =
+        heightsArray.indexOf(biggestColumnHeight);
+
+      const container = document.querySelector(".details_container");
+
+      if (!container) {
+        return;
+      }
+
+      (container as HTMLDivElement).style.height = `${
+        Math.floor(heightsArray[biggestColumnHeightIndex]) + 50
+      }px`;
+
+      heightsArray.forEach((element) => {
+        if (smallestColumnHeight === -1) {
+          smallestColumnHeight = element;
+        } else if (smallestColumnHeight > element) {
+          smallestColumnHeight = element;
+        }
+      });
+
+      const smallestColumnHeightIndex =
+        heightsArray.indexOf(smallestColumnHeight);
+
+      switch (smallestColumnHeightIndex) {
+        case 0:
+          (firstColumn as HTMLDivElement).appendChild(articles[n]);
+          break;
+        case 1:
+          (secondColumn as HTMLDivElement).appendChild(articles[n]);
+          break;
+        case 2:
+          (thirdColumn as HTMLDivElement).appendChild(articles[n]);
+      }
+
+      if (refContainer.current) {
+        (refContainer.current as HTMLDivElement).innerHTML = "";
+      }
+    }
+  }, [windowWidth, resListArray]);
+
   const pageComponent =
     resPage === null ? (
       <p>Error in getting page data</p>
     ) : (
-      <PageView resPage={resPage} />
+      <SubPageView edit={edit} resPage={resPage} remove={remove} />
     );
 
   const listComponent =
@@ -36,29 +138,99 @@ const DetailsView = ({ resListArray, resPage }: Props) => {
     ) : resListArray[0] === null ? (
       <p>Error in getting list data </p>
     ) : (
-      resListArray.map((list, index) => <ListView key={index} list={list} />)
+      true
     );
 
   return (
     <>
-      <button onClick={add}>Add</button>
-      {isAddViewVisible && (
-        <Modal isVisible={isAddViewVisible} setIsVisible={setIsAddViewVisible}>
-          <AddView />
-        </Modal>
-      )}
+      <>
+        {isAddViewVisible && (
+          <ComplexModal
+            isVisible={isAddViewVisible}
+            setIsVisible={setIsAddViewVisible}
+            buttonTitle={"Add New List"}
+          >
+            <AddView />
+          </ComplexModal>
+        )}
 
-      {isEditViewVisible && (
-        <Modal
-          isVisible={isEditViewVisible}
-          setIsVisible={setIsEditViewVisible}
-        >
-          <EditView />
-        </Modal>
-      )}
-      <button onClick={edit}>Edit</button>
-      <div>{pageComponent}</div>
-      <div>{listComponent}</div>
+        {isEditViewVisible && (
+          <ComplexModal
+            isVisible={isEditViewVisible}
+            setIsVisible={setIsEditViewVisible}
+            buttonTitle={"Edit List"}
+          >
+            <EditView />
+          </ComplexModal>
+        )}
+
+        {isRemoveViewVisible && (
+          <GenericModal
+            isVisible={isRemoveViewVisible}
+            setIsVisible={setIsRemoveViewVisible}
+          >
+            <RemoveView />
+          </GenericModal>
+        )}
+      </>
+      <div className="details_container">
+        <div>
+          <div ref={refFirstColumn} className="details_container-first_column">
+            {pageComponent}
+          </div>
+
+          <div
+            ref={refSecondColumn}
+            className="details_container-second_column"
+          >
+            {listComponent === true ? (
+              <ListView
+                key="list-1"
+                list={resListArray[0]}
+                edit={edit}
+                remove={remove}
+              />
+            ) : (
+              listComponent
+            )}
+          </div>
+
+          <div ref={refThirdColumn} className="details_container-third_column">
+            {
+              <ListView
+                key="list-2"
+                edit={edit}
+                list={resListArray[1]}
+                remove={remove}
+              />
+            }
+          </div>
+        </div>
+      </div>
+      <FooterView />
+      <div
+        style={{ display: "none" }}
+        ref={refContainer}
+        id="articles_template"
+      >
+        {resListArray.length >= 3 &&
+          resListArray
+            .slice(2, resListArray.length)
+            .map((list, index) => (
+              <ListView
+                key={index}
+                className="articles_template-item"
+                list={list}
+                edit={edit}
+                remove={remove}
+              />
+            ))}
+        <article className="articles_template-item">
+          <button className="button button_elevated" onClick={add}>
+            Add New List
+          </button>
+        </article>
+      </div>
     </>
   );
 };
