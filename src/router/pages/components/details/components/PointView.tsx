7ec3a97207eb902c25a69f706f5eedd47a66d483
brigss <brigitta.rucz@yahoo.com>
2023-04-26 18:13:01 +0300

    Add PointView with its styling

--- src/router/pages/components/details/components/PointView.tsx
+++ src/router/pages/components/details/components/PointView.tsx
@@ -2,13 +2,24 @@ import { Point } from "services/database/models";
 
 interface Props {
   point: Point;
+  styleClass: string;
 }
-const PointView = ({ point }: Props) => {
+const PointView = ({ point, styleClass }: Props) => {
   return (
-    <div>
-      <input type="checkbox" checked={point.checked} disabled />
-      <p>Additional Information: {point.additionalInformation}</p>
-      <p>Description: {point.description}</p>
+    <div className={`point_container ${styleClass}`}>
+      <div>
+        <div className="point_content-todo">
+          <input type="checkbox" checked={point.checked} />
+          <p className="text_body-2">{point.description}</p>
+        </div>
+        <p className="text_body-1 point_content-description">
+          {point.additionalInformation}
+        </p>
+      </div>
+      <div>
+        {point.images &&
+          point.images.map((image) => <img src={image} alt="Point" />)}
+      </div>
     </div>
   );
 };
