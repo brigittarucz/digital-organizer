890ef8c229d99ede1b2d087e69d10a3b135dd57a
brigss <brigitta.rucz@yahoo.com>
2023-04-27 15:43:46 +0300

    Add button filled tonal and outline styling

--- src/router/utils/Button.tsx
+++ src/router/utils/Button.tsx
@@ -1,11 +1,21 @@
+import { MouseEvent } from "react";
+
 interface Props {
   title?: string;
   icon?: string;
-  type: "filled" | "elevated" | "icon-outline" | "icon-filled" | "icon-generic";
-  onClick?: () => void;
+  type:
+    | "filled"
+    | "filled-tonal"
+    | "outline"
+    | "elevated"
+    | "icon-outline"
+    | "icon-filled"
+    | "icon-generic";
+  onClick?: (() => void) | ((ev: MouseEvent<HTMLElement>) => void);
   selected?: boolean;
   iconSelectedTrue?: string;
   iconSelectedFalse?: string;
+  submit?: true;
 }
 
 const Button = ({
@@ -16,18 +26,35 @@ const Button = ({
   selected,
   iconSelectedTrue,
   iconSelectedFalse,
+  submit,
 }: Props) => {
   const button = (() => {
     switch (type) {
       case "elevated":
         return (
-          <button className="button button_elevated" onClick={onClick}>
+          <button
+            type={`${submit ? "submit" : "button"}`}
+            className="button button_elevated"
+            onClick={onClick}
+          >
             {title}
           </button>
         );
       case "filled":
         return (
           <button className="button button_filled" onClick={onClick}>
+            {title}
+          </button>
+        );
+      case "filled-tonal":
+        return (
+          <button className="button button_filled--tonal" onClick={onClick}>
+            {title}
+          </button>
+        );
+      case "outline":
+        return (
+          <button className="button button_outline" onClick={onClick}>
             {title}
           </button>
         );
