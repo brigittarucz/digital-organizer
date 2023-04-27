80e87f448db5b42f9db404fe17e829526a59a37c
brigss <brigitta.rucz@yahoo.com>
2023-04-27 15:40:16 +0300

    Add edit-icon variants and split Modal.tsx into Complex & Generic

---
+++ src/router/utils/ComplexModal.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  isVisible: boolean;
+  setIsVisible: (visible: boolean) => void;
+}
+
+const ComplexModal = ({ children, isVisible, setIsVisible }: Props) => {
+  const close = () => {
+    setIsVisible(false);
+  };
+
+  return (
+    <div className="modal" style={{ display: isVisible ? "block" : "none" }}>
+      <div className="modal_content">
+        <section className="modal_content-grid">
+          <article className="modal_content-grid-children">{children}</article>
+          <article className="modal_content-grid-graphics">
+            <span className="close" onClick={close}>
+              X
+            </span>
+          </article>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default ComplexModal;
